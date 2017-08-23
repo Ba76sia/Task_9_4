60cc4bebac1507a5bb18172728f2be842cb54a78
Barbara Śmiech <b.smiech@wp.pl>
2017-08-23 07:40:09 +0200

    loops

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var drawTree = drawTree(10);
+    for (var i = 0 ; i < 10 ; i++ ); {
+        var star = string};
+            for (var j = 0 ; j < i++ ; j++ );{
+                var star = '*' };
+        console log(star);
