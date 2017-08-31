70b9b4c371c365e0c262cfe9a98ca8568206c3ff
Barbara Śmiech <b.smiech@wp.pl>
2017-08-31 20:43:30 +0200

    second loop

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,12 @@ function drawTree(levels) {
     var star = '';
     for (var i = 0; i < levels; i++) {
         var space = '';
-        star += '*';
+        star += '* ';
+        for (var j = levels; j > i; j--) {
+            space += ' ';
+        }
         console.log(space + star);
-        for (var j = levels; j > i; j--);
     } 
 }
+
 drawTree(10);
